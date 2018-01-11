3693a2e576db4d833a0324d7e0b7192a090b16c8
abiancu <alejandrobiancucci@gmail.com>
2018-01-11 16:05:30 -0600

    First Commit

--- TheBestLibraryCatalog/Program.cs
+++ TheBestLibraryCatalog/Program.cs
@@ -10,6 +10,75 @@ namespace TheBestLibraryCatalog
     {
         static void Main(string[] args)
         {
+
+
+            CardCatalog bookCatalog = new CardCatalog();
+            Console.WriteLine("Enter a file name");
+            bookCatalog.FileName = Console.ReadLine();
+            bookCatalog.MenuDisplay();
+
+            
+
+            Console.ReadLine();
+            
+
+           
+
+        }
+
+        public class CardCatalog
+        {
+            private string _filename;         
+            
+
+            
+
+            public string FileName
+            {
+                get
+                {
+                    return "_filename";
+                }
+                set
+                {
+                    _filename = value;
+                }
+            }
+            public void MenuDisplay()
+            {
+                Console.WriteLine("1. List All Books");
+                Console.WriteLine("2. Add a Book");
+                Console.WriteLine("3. Save and Exit");
+                Console.WriteLine("What would you like to do?");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                MenuDisplayLogic(userInput); 
+                
+                
+            }
+
+            private static void MenuDisplayLogic(int userInput)
+            {
+                
+                if (userInput == 1)
+                {
+                    Console.WriteLine("You've enterd 1");
+
+                    //run method ListAllBooks()
+                    // call MenuDisplay at the end of choice 1
+                }
+                if (userInput == 2)
+                {
+                    Console.WriteLine("you've enterd 2");
+                    // run method AddABook()
+                    // call MeneDisplay at the en of chocice 2
+                }
+                if (userInput == 3)
+                {
+                    Console.WriteLine("you've enterd 3");
+                    // run method SaveAndExit()
+                }
+            }
         }
     }
 }
